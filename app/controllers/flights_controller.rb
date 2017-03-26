d551d38eeda7d57ed9fc87be75e869f155676f22
Len Lee <lenlee454@gmail.com>
2017-03-26 14:19:28 -0700

    add flight controller, index action, and form

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,8 @@
+class FlightsController < ApplicationController
+
+  def index
+    @airport_options = Airport.all.map{ |i| [i.code, i.id] }
+    @date_options = Flight.all.sorted.map{ |n| [n.date_formatted, n.id] }
+  end
+
+end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,12 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: "Airport"
   belongs_to :to_airport, class_name: "Airport"
+
+  def date_formatted
+    self.start_time.strftime("%a %b %d, %Y - %l:%M%P")
+  end
+
+  def self.sorted
+    self.order(:start_time) 
+  end
 end
