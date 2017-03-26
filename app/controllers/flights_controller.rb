433c3f7e6fd96357d77bbbea9bab122f312db5e8
Len Lee <lenlee454@gmail.com>
2017-03-26 16:38:35 -0700

    add result form on index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,8 @@ class FlightsController < ApplicationController
 
   def index
     @airport_options = Airport.all.map{ |i| [i.code, i.id] }
-    @date_options = Flight.all.sorted.map{ |n| [n.date_formatted, n.id] }
+    @date_options = Flight.all.sorted.map{ |n| [n.date_formatted, n.start_time] }
+    @flights = Flight.search(params)
   end
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,10 +3,19 @@ class Flight < ApplicationRecord
   belongs_to :to_airport, class_name: "Airport"
 
   def date_formatted
-    self.start_time.strftime("%a %b %d, %Y - %l:%M%P")
+    self.start_time.strftime("%a %b %d, %Y")
   end
 
   def self.sorted
-    self.order(:start_time) 
+    self.order(:start_time)
   end
+
+  def self.search(params)
+    if params
+      Flight.where(from_airport_id: params[:from_airport], to_airport_id: params[:to_airport], start_time: params[:start_time])
+    else
+      Flight.none
+    end
+  end
+
 end
